4487b1bb23da327e8c5862a6117c168692da8591
lujingkang <443911605@qq.com>
2024-09-12 01:11:40 +0800

    event log

--- src/views/base/eventlog/eventLog.data.ts
+++ src/views/base/eventlog/eventLog.data.ts
@@ -1,112 +1,69 @@
-import type {BasicColumn, FormSchema} from '@/components/Table'
-import {useRender} from '@/components/Table'
+import type { BasicColumn, FormSchema } from '@/components/Table'
+import { useRender } from '@/components/Table'
 
 export const columns: BasicColumn[] = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        width: 160,
+
+  {
+    title: '控制号',
+    dataIndex: 'eventId',
+    width: 160,
+  },
+  {
+    title: '异常措施',
+    dataIndex: '',
+    width: 160,
+  },
+  {
+    title: '触发类型',
+    dataIndex: 'eventType',
+    width: 160,
+  },
+  {
+    title: '来源',
+    dataIndex: '',
+    width: 160,
+  },
+  {
+    title: '执行结果',
+    dataIndex: '',
+    width: 160,
+  },
+  {
+    title: '创建人员',
+    dataIndex: 'creator',
+    width: 160,
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    width: 180,
+    customRender: ({ text }) => {
+      return useRender.renderDate(text)
     },
-        {
-          title: '设备编码',
-          dataIndex: 'eqptCode',
-          width: 160,
-        },
-        {
-          title: '事件ID',
-          dataIndex: 'eventId',
-          width: 160,
-        },
-        {
-          title: '事件代码',
-          dataIndex: 'eventCode',
-          width: 160,
-        },
-        {
-          title: '全局事件代码',
-          dataIndex: 'eventGlobaleCode',
-          width: 160,
-        },
-        {
-          title: '事件类型',
-          dataIndex: 'eventType',
-          width: 160,
-        },
-        {
-          title: '发生时间',
-          dataIndex: 'occrTime',
-          width: 180,
-          customRender: ({ text }) => {
-            return useRender.renderDate(text)
-          },
-        },
-        {
-          title: '部门id',
-          dataIndex: 'deptId',
-          width: 160,
-        },
-        {
-          title: '创建时间',
-          dataIndex: 'createTime',
-          width: 180,
-          customRender: ({ text }) => {
-            return useRender.renderDate(text)
-          },
-        },
+  },
 ]
 
 export const searchFormSchema: FormSchema[] = [
-      {
-        label: '设备编码',
-        field: 'eqptCode',
-          component: 'Input',
-        colProps: { span: 8 },
-      },
-      {
-        label: '事件ID',
-        field: 'eventId',
-          component: 'Input',
-        colProps: { span: 8 },
-      },
-      {
-        label: '事件代码',
-        field: 'eventCode',
-          component: 'Input',
-        colProps: { span: 8 },
-      },
-      {
-        label: '全局事件代码',
-        field: 'eventGlobaleCode',
-          component: 'Input',
-        colProps: { span: 8 },
-      },
-      {
-        label: '事件类型',
-        field: 'eventType',
-          component: 'Select',
-          componentProps: {
-              options: [],
-          },
-        colProps: { span: 8 },
-      },
-      {
-        label: '发生时间',
-        field: 'occrTime',
-          component: 'RangePicker',
-        colProps: { span: 8 },
-      },
-      {
-        label: '部门id',
-        field: 'deptId',
-          component: 'Input',
-        colProps: { span: 8 },
-      },
-      {
-        label: '创建时间',
-        field: 'createTime',
-          component: 'RangePicker',
-        colProps: { span: 8 },
-      },
+  {
+    label: ' ',
+    labelWidth: 15,
+    field: '',
+    component: 'Input',
+    componentProps: {
+      placeholder: '项目名称'
+    },
+    colProps: { span: 5 },
+  },
+  {
+    label: ' ',
+    labelWidth: 15,
+    field: '',
+    component: 'Input',
+    componentProps: {
+      placeholder: '项目代码'
+    },
+    colProps: { span: 5 },
+  },
 ]
 
 export const createFormSchema: FormSchema[] = [
@@ -116,49 +73,49 @@ export const createFormSchema: FormSchema[] = [
     show: false,
     component: 'Input'
   },
-        {
-          label: '设备编码',
-          field: 'eqptCode',
-            component: 'Input',
-        },
-        {
-          label: '事件ID',
-          field: 'eventId',
-            component: 'Input',
-        },
-        {
-          label: '事件代码',
-          field: 'eventCode',
-            component: 'Input',
-        },
-        {
-          label: '全局事件代码',
-          field: 'eventGlobaleCode',
-            component: 'Input',
-        },
-        {
-          label: '事件类型',
-          field: 'eventType',
-            component: 'Select',
-            componentProps: {
-                options:[],
-            },
-        },
-        {
-          label: '发生时间',
-          field: 'occrTime',
-            component: 'DatePicker',
-            componentProps: {
-              showTime: true,
-              format: 'YYYY-MM-DD HH:mm:ss',
-              valueFormat: 'x',
-            },
-        },
-        {
-          label: '部门id',
-          field: 'deptId',
-            component: 'Input',
-        },
+  {
+    label: '设备编码',
+    field: 'eqptCode',
+    component: 'Input',
+  },
+  {
+    label: '事件ID',
+    field: 'eventId',
+    component: 'Input',
+  },
+  {
+    label: '事件代码',
+    field: 'eventCode',
+    component: 'Input',
+  },
+  {
+    label: '全局事件代码',
+    field: 'eventGlobaleCode',
+    component: 'Input',
+  },
+  {
+    label: '事件类型',
+    field: 'eventType',
+    component: 'Select',
+    componentProps: {
+      options: [],
+    },
+  },
+  {
+    label: '发生时间',
+    field: 'occrTime',
+    component: 'DatePicker',
+    componentProps: {
+      showTime: true,
+      format: 'YYYY-MM-DD HH:mm:ss',
+      valueFormat: 'x',
+    },
+  },
+  {
+    label: '部门id',
+    field: 'deptId',
+    component: 'Input',
+  },
 ]
 
 export const updateFormSchema: FormSchema[] = [
@@ -168,47 +125,47 @@ export const updateFormSchema: FormSchema[] = [
     show: false,
     component: 'Input'
   },
-        {
-          label: '设备编码',
-          field: 'eqptCode',
-            component: 'Input',
-        },
-        {
-          label: '事件ID',
-          field: 'eventId',
-            component: 'Input',
-        },
-        {
-          label: '事件代码',
-          field: 'eventCode',
-            component: 'Input',
-        },
-        {
-          label: '全局事件代码',
-          field: 'eventGlobaleCode',
-            component: 'Input',
-        },
-        {
-          label: '事件类型',
-          field: 'eventType',
-            component: 'Select',
-            componentProps: {
-                options:[],
-            },
-        },
-        {
-          label: '发生时间',
-          field: 'occrTime',
-            component: 'DatePicker',
-            componentProps: {
-              showTime: true,
-              format: 'YYYY-MM-DD HH:mm:ss',
-              valueFormat: 'x',
-            },
-        },
-        {
-          label: '部门id',
-          field: 'deptId',
-            component: 'Input',
-        },
+  {
+    label: '设备编码',
+    field: 'eqptCode',
+    component: 'Input',
+  },
+  {
+    label: '事件ID',
+    field: 'eventId',
+    component: 'Input',
+  },
+  {
+    label: '事件代码',
+    field: 'eventCode',
+    component: 'Input',
+  },
+  {
+    label: '全局事件代码',
+    field: 'eventGlobaleCode',
+    component: 'Input',
+  },
+  {
+    label: '事件类型',
+    field: 'eventType',
+    component: 'Select',
+    componentProps: {
+      options: [],
+    },
+  },
+  {
+    label: '发生时间',
+    field: 'occrTime',
+    component: 'DatePicker',
+    componentProps: {
+      showTime: true,
+      format: 'YYYY-MM-DD HH:mm:ss',
+      valueFormat: 'x',
+    },
+  },
+  {
+    label: '部门id',
+    field: 'deptId',
+    component: 'Input',
+  },
 ]
--- src/views/base/eventlog/index.vue
+++ src/views/base/eventlog/index.vue
@@ -1,89 +1,87 @@
 <script lang="ts" setup>
-  import EventLogModal from './EventLogModal.vue'
-  import { columns, searchFormSchema } from './eventLog.data'
-  import { useI18n } from '@/hooks/web/useI18n'
-  import { useMessage } from '@/hooks/web/useMessage'
-  import { useModal } from '@/components/Modal'
-  import { IconEnum } from '@/enums/appEnum'
-  import { BasicTable, TableAction, useTable } from '@/components/Table'
-  import { deleteEventLog, exportEventLog, getEventLogPage } from '@/api/base/eventlog'
+import EventLogModal from './EventLogModal.vue'
+import { columns, searchFormSchema } from './eventLog.data'
+import { useI18n } from '@/hooks/web/useI18n'
+import { useMessage } from '@/hooks/web/useMessage'
+import { useModal } from '@/components/Modal'
+import { IconEnum } from '@/enums/appEnum'
+import { BasicTable, TableAction, useTable } from '@/components/Table'
+import { deleteEventLog, exportEventLog, getEventLogPage } from '@/api/base/eventlog'
 
-  defineOptions({ name: 'EventLog' })
+defineOptions({ name: 'EventLog' })
 
-  const { t } = useI18n()
-  const { createConfirm, createMessage } = useMessage()
-  const [registerModal, { openModal }] = useModal()
+const { t } = useI18n()
+const { createConfirm, createMessage } = useMessage()
+const [registerModal, { openModal }] = useModal()
 
-  const [registerTable, { getForm, reload }] = useTable({
-    title: '设备事件日志列表',
-    api: getEventLogPage,
-    columns,
-    formConfig: { labelWidth: 120, schemas: searchFormSchema },
-    useSearchForm: true,
-    showTableSetting: true,
-    actionColumn: {
-      width: 140,
-      title: t('common.action'),
-      dataIndex: 'action',
-      fixed: 'right',
-    }
+const [registerTable, { getForm, reload }] = useTable({
+  title: '设备事件日志列表',
+  api: getEventLogPage,
+  columns,
+  formConfig: { labelWidth: 120, schemas: searchFormSchema },
+  useSearchForm: true,
+  showTableSetting: true,
+  // actionColumn: {
+  //   width: 140,
+  //   title: t('common.action'),
+  //   dataIndex: 'action',
+  //   fixed: 'right',
+  // }
+})
+
+function handleCreate() {
+  openModal(true, { isUpdate: false })
+}
+
+function handleEdit(record: Recordable) {
+  openModal(true, { record, isUpdate: true })
+}
+
+async function handleExport() {
+  createConfirm({
+    title: t('common.exportTitle'),
+    iconType: 'warning',
+    content: t('common.exportMessage'),
+    async onOk() {
+      await exportEventLog(getForm().getFieldsValue())
+      createMessage.success(t('common.exportSuccessText'))
+    },
   })
+}
 
-  function handleCreate() {
-    openModal(true, { isUpdate: false })
-  }
-
-  function handleEdit(record: Recordable) {
-    openModal(true, { record, isUpdate: true })
-  }
-
-  async function handleExport() {
-    createConfirm({
-      title: t('common.exportTitle'),
-      iconType: 'warning',
-      content: t('common.exportMessage'),
-      async onOk() {
-        await exportEventLog(getForm().getFieldsValue())
-        createMessage.success(t('common.exportSuccessText'))
-      },
-    })
-  }
-
-  async function handleDelete(record: Recordable) {
-    await deleteEventLog(record.id)
-    createMessage.success(t('common.delSuccessText'))
-    reload()
-  }
+async function handleDelete(record: Recordable) {
+  await deleteEventLog(record.id)
+  createMessage.success(t('common.delSuccessText'))
+  reload()
+}
 </script>
 <template>
   <div>
     <BasicTable @register="registerTable">
-      <template #toolbar>
+      <!-- <template #toolbar>
         <a-button type="primary" v-auth="['base:event-log:create']" :preIcon="IconEnum.ADD" @click="handleCreate">
           {{ t('action.create') }}
         </a-button>
         <a-button v-auth="['base:event-log:export']" :preIcon="IconEnum.EXPORT" @click="handleExport">
           {{ t('action.export') }}
         </a-button>
-      </template>
+      </template> -->
       <template #bodyCell="{ column, record }">
         <template v-if="column.key === 'action'">
-          <TableAction
-                  :actions="[
-                          { icon: IconEnum.EDIT, label: t('action.edit'), auth: 'base:event-log:update', onClick: handleEdit.bind(null, record) },
-                          {
-                          icon: IconEnum.DELETE,
-                          danger: true,
-                          label: t('action.delete'),
-                          auth: 'base:event-log:delete',
-                          popConfirm: {
-                          title: t('common.delMessage'),
-                          placement: 'left',
-                          confirm: handleDelete.bind(null, record),
-                          },
-                          },
-                          ]"
-          />
+          <TableAction :actions="[
+            { icon: IconEnum.EDIT, label: t('action.edit'), auth: 'base:event-log:update', onClick: handleEdit.bind(null, record) },
+            {
+              icon: IconEnum.DELETE,
+              danger: true,
+              label: t('action.delete'),
+              auth: 'base:event-log:delete',
+              popConfirm: {
+                title: t('common.delMessage'),
+                placement: 'left',
+                confirm: handleDelete.bind(null, record),
+              },
+            },
+          ]" />
         </template>
       </template>
     </BasicTable>
