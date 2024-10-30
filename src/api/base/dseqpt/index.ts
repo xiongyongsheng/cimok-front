2af532dd12cab351241049e62db341ce04dc0174
ultraman0 <5322146+ultrazz@user.noreply.gitee.com>
2024-10-30 10:34:08 +0800

    修改 调用地址

--- src/api/base/dseqpt/index.ts
+++ src/api/base/dseqpt/index.ts
@@ -2,5 +2,5 @@ import { defHttp } from '@/utils/http/axios'
 
 // 查询设备台账列表
 export function getDSEqptPage(params) {
-  return defHttp.get({ url: '/ds/ds-event-log/page', params })
+  return defHttp.get({ url: '/base/ds-event-log/page', params })
 }
